1e3770da59c0cf0c0689a2b65064084afcee57df
Minodix <59900326+Minodix@users.noreply.github.com>
2020-01-25 00:15:17 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumaDeImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, impar = 0, sumaI = 0;
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                   // Console.Write("{0},", i);
+                    sumaI += i;
+                }
+            }
+            Console.Write("La suma de los numeros impares es: " + sumaI);
+            Console.ReadKey();
+        }
+    }
+}
